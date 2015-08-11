53dd00d3673e0d5828a676e0999bcabc5f4adae1
Tanner <tannert8155@gmail.com>
2015-08-11 13:07:16 -0500

    AJAX for posting to firebase added

--- javascripts/main.js
+++ javascripts/main.js
@@ -27,6 +27,13 @@ requirejs(
         method: "GET"
       }).done(function(data){
         console.log(data)
+        $.ajax({
+          url: "https://movie-application.firebaseio.com/.json",
+          method: "POST",
+          data: JSON.stringify(data)
+        }).done(function(){
+          console.log("loaded to firebase");
+          });
       })
     });
     
