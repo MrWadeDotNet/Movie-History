d857436aa20db661c31a67472fb83ff8be2f8fbe
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-12 12:57:08 -0500

    added posters

--- javascripts/retrieveAndLoad.js
+++ javascripts/retrieveAndLoad.js
@@ -7,6 +7,7 @@ define(function(){
         method: "GET"
       }).done(function(data){
         data.userRating = 0;
+        data.poster = "http://img.omdbapi.com/?i=" + data.imdbID + "&apikey=8513e0a1";
   //      data.watched = false;
         console.log(data);
         $.ajax({
