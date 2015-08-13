3892172d11d69173ba1222047a11e770beaf6e47
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-13 09:34:54 -0500

    beautified code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,3 @@
-
 requirejs.config({
   baseUrl: './javascripts',
   paths: {
@@ -6,85 +5,86 @@ requirejs.config({
     'hbs': '../bower_components/require-handlebars-plugin/hbs',
     'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
     'firebase': '../bower_components/firebase/firebase',
-    'lodash' : '../bower_components/lodash/lodash',
+    'lodash': '../bower_components/lodash/lodash',
   },
   shim: {
     'bootstrap': ['jquery'],
     'firebase': {
-        exports: 'Firebase'
+      exports: 'Firebase'
     }
   }
 });
 
 requirejs(
-  ["jquery","lodash","firebase", "hbs", "bootstrap", "retrieveAndLoad", "display","viewed","remove", "rating"],
-  function($, _, _firebase, Handlebars, bootstrap, RAL, display,addViewed,deleteFromDb, addRating) {
+  ["jquery", "lodash", "firebase", "hbs", "bootstrap", "retrieveAndLoad", "display", "viewed", "remove", "rating"],
+  function($, _, _firebase, Handlebars, bootstrap, RAL, display, addViewed, deleteFromDb, addRating) {
     var myFirebaseRef = new Firebase("https://movie-application.firebaseio.com/");
     myFirebaseRef.child("movies").on("value", function(snapshot) {
       console.log(snapshot.val());
       var movies = snapshot.val();
       console.log(movies);
-      var allMoviesArray=[];
-       // Convert Firebase's object of objects into an array of objects
+      var allMoviesArray = [];
+      // Convert Firebase's object of objects into an array of objects
       for (var key in movies) {
         allMoviesArray[allMoviesArray.length] = movies[key];
       }
-      allMoviesObject = {movies: allMoviesArray};
+      allMoviesObject = {
+        movies: allMoviesArray
+      };
 
-      display(movies);  
+      display(movies);
 
     });
 
-    $('#submitmovie').on("click", function(){
+    $('#submitmovie').on("click", function() {
       RAL();
-      });
+    });
 
-   $(document).on("click", '.isviewed', function() {
-    var datakey = ($(this).parent().attr('data-key'));
+    $(document).on("click", '.isviewed', function() {
+      var datakey = ($(this).parent().attr('data-key'));
 
       addViewed(datakey);
-       });
+    });
 
-   $(document).on("click", '.delete', function() {
-    var datakey = ($(this).parent().attr('data-key'));
-    console.log(datakey);
+    $(document).on("click", '.delete', function() {
+      var datakey = ($(this).parent().attr('data-key'));
+      console.log(datakey);
       deleteFromDb(datakey);
-       });
+    });
 
-   $(document).on("click", '.rating', function() {
-    console.log("this button works");
-    var datakey = ($(this).parent().attr('data-key'));
-    var input = $(this).prev().val();
+    $(document).on("click", '.rating', function() {
+      console.log("this button works");
+      var datakey = ($(this).parent().attr('data-key'));
+      var input = $(this).prev().val();
 
       addRating(datakey, input);
-       });
+    });
 
     $(document).on("click", '.moviedata', function() {
       var datakey = ($(this).attr('data-key'));
       console.log(datakey);
-       var myFirebaseRef = new Firebase("https://movie-application.firebaseio.com/movies/");
-       myFirebaseRef.once("value", function(snapshot) {
+      var myFirebaseRef = new Firebase("https://movie-application.firebaseio.com/movies/");
+      myFirebaseRef.once("value", function(snapshot) {
         var movies = snapshot.val();
-      
+
         $(".actors").html(movies[datakey].Actors);
         $(".released").html(movies[datakey].Released);
-     // console.log(movies[datakey].watched);
-     //Check for Watched Movie
-     if(movies[datakey].watched === true) {
+        // console.log(movies[datakey].watched);
+        //Check for Watched Movie
+        if (movies[datakey].watched === true) {
           watchedText = "Yes";
+        } else {
+          watchedText = "No";
         }
-         else {
-           watchedText = "No";
-          }
 
-       $(".iswatched").html(watchedText);
-       });
-    });  
+        $(".iswatched").html(watchedText);
+      });
+    });
 
-  /*  $(document).on("click", ".moviedata", function() { 
+    /*  $(document).on("click", ".moviedata", function() { 
       console.log(this);
     $(".bottom-container").toggleClass('show');
     $(".bottom-container").toggleClass('hide');
 }); */
-  
- });
+
+  });
--- javascripts/rating.js
+++ javascripts/rating.js
@@ -1,10 +1,10 @@
-define(function(keydata, userInput){
-  return function(keydata, userInput){
-      
-      console.log(keydata);
-      var firebaseUpdate = new Firebase("https://movie-application.firebaseio.com/movies/" + keydata);
-       firebaseUpdate.child("userRating").set(userInput);
-        console.log("Updated");
+define(function(keydata, userInput) {
+  return function(keydata, userInput) {
+
+    console.log(keydata);
+    var firebaseUpdate = new Firebase("https://movie-application.firebaseio.com/movies/" + keydata);
+    firebaseUpdate.child("userRating").set(userInput);
+    console.log("Updated");
     //   addViewed(datakey);
   };
-    });
+});
